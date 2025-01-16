60871aeaf9d9d91962c2417a9146a2a4529fb2d2
daniele.gattuso <d.gattuso09@gmail.com>
2025-01-16 15:07:11 +0100

    continue 1st excercize

--- esercizio 1/js/script.js
+++ esercizio 1/js/script.js
@@ -2,4 +2,51 @@
 
 // Snack 1
 // Creare un array di oggetti: Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso. Stampare a schermo la bici con peso minore.
-// Snack2
+
+
+
+const biciDaCorsa = [
+    { nome: "Bianchi Oltre XR4", peso: 9 },
+    { nome: "Specialized Tarmac SL7", peso: 8 },
+    { nome: "Pinarello Dogma F", peso: 6 },
+    { nome: "Cannondale SuperSix EVO", peso: 7 },
+    { nome: "Trek Émonda SLR", peso: 5 }
+  ];
+
+
+// Stampo tutti gli elementi di un array
+
+for(let i = 0; i< biciDaCorsa.length; i++){
+
+    console.log(biciDaCorsa[i]);
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Creo una funzione per generare un numero casuale
+
+function randomNumber(min, max) {
+
+    return Math.floor(Math.random() *max + min) 
+ }
+
+  let casuale = randomNumber(1, 10)
+ 
+  
